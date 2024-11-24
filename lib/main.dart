81847ff1b554e13cb623aeea880d419d5e82858e
yousefmohamed35 <yousefmo2021jo@gmail.com>
2024-11-24 14:58:15 +0200

    create app

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const TaskApp());
+}
+class TaskApp extends StatelessWidget {
+  const TaskApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
+
